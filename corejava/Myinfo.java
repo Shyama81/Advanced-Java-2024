bafa01be40db9626bf4a6ffe73cc7fb8e1dc7cb3
Shyama Mainali <mainalishyama@gmail.com>
2024-05-31 17:48:10 +0545

    my first java program!

---
+++ corejava/Myinfo.java
@@ -0,0 +1,9 @@
+package corejava;
+
+public class Myinfo {
+public static void main(String[] args) {
+	System.out.println("Shyama Mainali");
+	System.out.println("kan077bct081");
+	System.out.println("New Baneshwor"); 
+}
+}
